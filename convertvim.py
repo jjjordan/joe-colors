44addbebf86cd1e97e6a7976a49f9e04ed030b65
John J. Jordan <jj@jjjordan.io>
2021-04-27 10:45:29 -0700

    Add -visiblews to vim conversion

--- convertvim.py
+++ convertvim.py
@@ -70,6 +70,7 @@ CONVERT = {
     "pmenu":		["-menu"],
     "pmenusel":		["-menusel"],
     "cursor":		["-cursor"],
+    "specialkey":	["-visiblews"],
     
     "comment":		["=Comment"],
     "constant":		["=Constant"],
@@ -127,7 +128,7 @@ CONVERT = {
 }
 
 ORDER = [
-    "-text", "-status", "-selection", "-linum", "-curlin", "-curlinum", "-menu", "-menusel", "-cursor",
+    "-text", "-status", "-selection", "-linum", "-curlin", "-curlinum", "-menu", "-menusel", "-visiblews", "-cursor",
     "",
     "-term 0", "-term 1", "-term 2", "-term 3", "-term 4", "-term 5", "-term 6", "-term 7",
     "-term 8", "-term 9", "-term 10", "-term 11", "-term 12", "-term 13", "-term 14", "-term 15",
